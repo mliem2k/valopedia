8c6ef70406e1d7585b582412280450359af81f74
michael.liem <michael.liem@primecash.co.id>
2022-09-10 17:16:50 +0700

    image

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ class App extends Component {
     return (
       <div className="App">
         <Header resumeData={resumeData}/>
-{/*         <About resumeData={resumeData}/>
-        <Resume resumeData={resumeData}/>
+   {/*     <About resumeData={resumeData}/>
+         <Resume resumeData={resumeData}/>
         <Portfolio resumeData={resumeData}/>
         <Testimonials resumeData={resumeData}/>
         <ContactUs resumeData={resumeData}/>
