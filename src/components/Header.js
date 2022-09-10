1463331c8a0bc7774ae2fd99d297ce77da734e44
michael.liem <michael.liem@primecash.co.id>
2022-09-10 17:03:35 +0700

    remove all except header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export default class Header extends Component {
       <React.Fragment>
       
       <header id="home">
-         <nav id="nav-wrap">
+ {/*         <nav id="nav-wrap">
             <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
           <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
             <ul id="nav" className="nav">
@@ -17,7 +17,7 @@ export default class Header extends Component {
                <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
                <li><a className="smoothscroll" href="#contact">Contact</a></li>
             </ul>
-         </nav>
+         </nav> */}
 
          <div className="row banner">
             <div className="banner-text">
@@ -40,9 +40,9 @@ export default class Header extends Component {
             </div>
          </div>
 
-         <p className="scrolldown">
+{/*          <p className="scrolldown">
             <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
-         </p>
+         </p> */}
 
       </header>
       </React.Fragment>
